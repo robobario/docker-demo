d46e30521f5e00857cbb0d50dc40d9f5166175a2
roby <rob.young@adscale.co.nz>
2016-08-26 09:26:41 +1200

    Remove sleep from test

--- test-python/test.py
+++ test-python/test.py
@@ -5,7 +5,6 @@ import time
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
-        time.sleep(3)
         r = requests.get('http://hello.world:8000')
         self.assertEqual(r.text, 'Hello, world')
 
